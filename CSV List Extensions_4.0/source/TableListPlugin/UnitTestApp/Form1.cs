afe1f581028731ce8e10364363de0e60707534b1
Theodore Gaushas <webmaster@tmg-corporation.com>
2017-04-27 21:50:42 -0400

    Add files via upload

---
+++ CSV List Extensions_4.0/source/TableListPlugin/UnitTestApp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TableListCommands;
+
+namespace UnitTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string value = @"Hallo;Peter;Maier;Guenter;Peter;Herbert
+test;123"; 
+            //File.ReadAllText("sample.txt");
+            PluginInfo pinfo = new PluginInfo();
+            List<string> lstValue = pinfo.NormalizeString(value);
+            TableCSVGrid _wizardWindow = new TableCSVGrid();
+            _wizardWindow.csvFileContent = value;
+            _wizardWindow.rows = lstValue;
+            _wizardWindow.containsHeader = true;
+            _wizardWindow.delimiter = ';';
+            _wizardWindow.Title = "Title goes here";
+            _wizardWindow.Show();
+            //TableRowInsert dlg = new TableRowInsert();
+            //AddNewColumn dlg = new AddNewColumn();
+//            dlg.containsHeader = false;
+//            dlg.delimiter = ',';
+//            dlg.MakeReadOnly(false);
+            
+//            dlg.csvFileContent = @"h1,h2,h3
+//            l1,l2,l3
+//            l4,l5,l6";
+            
+//            dlg.Show();
+            
+        }
+    }
+}
---
+++ CSV List Extensions_4.0/source/TableListPlugin/WizardCommands/PluginInfo.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TableListCommands
+{
+    public partial class PluginInfo
+    {
+        public static string HashCode
+        {
+            get
+            {
+                return "ae3f68f3d87465414d8ce7981c923f7719eb224a";
+            }
+        }
+        public List<String> ToCellItems(String row, char delimiter)
+        {
+            List<String> cellItems = new List<string>();
+            bool quoteStated = false;
+            StringBuilder cell = new StringBuilder();
+            foreach (char c in row)
+            {
+                if (c == '"')
+                {
+                    quoteStated = !quoteStated;
+                    cell.Append(c);
+                }
+                else if (c == delimiter)
+                {
+                    if (quoteStated)
+                    {
+                        cell.Append(c);
+                    }
+                    else
+                    {
+                        cellItems.Add(cell.ToString());
+                        cell.Clear();
+                    }
+                }
+                else
+                {
+                    cell.Append(c);
+                }
+            }
+            if (cell.ToString().Trim().Length > 0) 
+            {
+                cellItems.Add(cell.ToString().Trim());
+                cell.Clear();
+            }
+            return cellItems;
+        }
+        public List<string> NormalizeString(String input)
+        {
+            List<string> rows = new List<string>();
+            bool quoteStarted = false;
+            StringBuilder row = new StringBuilder();
+            foreach (char c in input)
+            {
+                //If char is quote and quote hasn't been quoted yet then start recording row content.
+                if (c == '"')
+                {
+                    quoteStarted = !quoteStarted;
+                    row.Append(c);
+                }
+                else if (c == '\n')
+                {
+                    if (quoteStarted)
+                    {
+                        row.Append(c);
+                    }
+                    else
+                    {
+                        row.Append(c);
+                        if (row.ToString().Trim().Length > 0)
+                        {
+                            rows.Add(row.ToString());
+                        }
+                        row.Clear();
+                    }
+                }
+                else
+                {
+                    row.Append(c);
+                }
+            }
+            if (row.ToString().Trim().Length > 0)
+            {
+                rows.Add(row.ToString());
+                row.Clear();
+            }
+            return rows;
+        }
+    }
+}
